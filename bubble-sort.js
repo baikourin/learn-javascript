33d5a1cbca92dd5b90a1c84d477ae537b655990b
baikourin <guanglinmei@yahoo.com>
2021-12-29 21:26:10 +0900

    [add] comment

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,3 +1,5 @@
 /* 
     Bubble sort
+    The first layer of loop controls how many loops of bubbling (n - 1) are required in total. Each round will bubble up a maximum number to the end of the array
+     The second layer of loop starts from the leftmost v[0] every time, and compares it with the element to the right of it.
  */
