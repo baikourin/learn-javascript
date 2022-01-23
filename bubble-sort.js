ba18d610960c4145fb5bd9d5108602198db31856
baikourin <guanglinmei@yahoo.com>
2022-01-24 00:48:46 +0900

    [add] bubble sort algorithm

--- bubble-sort.js
+++ bubble-sort.js
@@ -3,3 +3,22 @@
     The first layer of loop controls how many loops of bubbling (n - 1) are required in total. Each round will bubble up a maximum number to the end of the array
      The second layer of loop starts from the leftmost v[0] every time, and compares it with the element to the right of it.
  */
+
+let arr = [10, 9, 8, 7, 6, 5, 3, 4, 2, 1, 1, 3, 2, 3, 2];
+
+function bubbleSort(arr){
+    let temp;
+    let len = arr.length;
+    for(let i = 1; i <= len - 1; i++){
+        for(let j = 0; j < len - i; j++){
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(bubbleSort(arr));
